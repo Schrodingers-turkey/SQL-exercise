2ead6bbf66a7c2548b75c516dc1f92b1d93b7ad4
Schrodingers-turkey <48345610+Schrodingers-turkey@users.noreply.github.com>
2019-11-24 17:54:15 +0800

    Update 2_questions.sql

--- SQL_exercise_02/2_questions.sql
+++ SQL_exercise_02/2_questions.sql
@@ -66,13 +66,57 @@ on a.department = b.code;
   select * from Departments
   where budget > (select avg(budget) from Departments)
   ;
--- 2.14 Select the names of departments with more than two employees.
-
+-- 2.14 Select the names of departments with more than two employees.(hard)
+  select name from Departments
+  where code in 
+    (
+      select department from Employees
+      group by department
+      having count(*)>2
+        );
+        
 -- 2.15 Very Important - Select the name and last name of employees working for departments with second lowest budget.
+  select name ,lastname from Employees 
+  where department =
+    (
+      select t.code from 
+        (
+          select * from Departments order by budget limit 2
+        )
+      t order by t.budget desc limit 1
+    );
+    
+    select name, lastname
+from Employees
+where department =(
+select temp.code 
+from (select * from Departments order by budget limit 2) temp
+order by temp.budget desc limit 1
+);
+
+/* With subquery */
+SELECT e.Name, e.LastName
+FROM Employees e 
+WHERE e.Department = (
+       SELECT sub.Code 
+       FROM (SELECT * FROM Departments d ORDER BY d.budget LIMIT 2) sub 
+       ORDER BY budget DESC LIMIT 1);
+    
 -- 2.16  Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. 
+  Insert into Departments value (11,"Quality Assurance",40000);
+  
 -- And Add an employee called "Mary Moore" in that department, with SSN 847-21-9811.
+  Insert into Employees value (847-21-9811,"Mary","Moore",11);
+  update Employees set SSN =847219811 where SSN= -8985;
+  
 -- 2.17 Reduce the budget of all departments by 10%.
+  update Departments set budget = budget *0.9;
+  
 -- 2.18 Reassign all employees from the Research department (code 77) to the IT department (code 14).
+  update Employees set department =14 where department = 77;
+  
 -- 2.19 Delete from the table all employees in the IT department (code 14).
+  delete from Employees where department = 14;
 -- 2.20 Delete from the table all employees who work in departments with a budget greater than or equal to $60,000.
+  delete 
 -- 2.21 Delete from the table all employees.
