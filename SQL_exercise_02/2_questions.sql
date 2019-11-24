373587c7cdca76b2bbd1a5e89fc7b62703de3630
Schrodingers-turkey <48345610+Schrodingers-turkey@users.noreply.github.com>
2019-11-24 10:49:44 +0800

    Update 2_questions.sql

--- SQL_exercise_02/2_questions.sql
+++ SQL_exercise_02/2_questions.sql
@@ -1,15 +1,62 @@
 -- LINK : https://en.wikibooks.org/wiki/SQL_Exercises/Employee_management
 -- 2.1 Select the last name of all employees.
+  select lastname from Employees;
 -- 2.2 Select the last name of all employees, without duplicates.
+  select distinct lastname from Employees;
 -- 2.3 Select all the data of employees whose last name is "Smith".
+  select * 
+  from Employees 
+  where lastname = "Smith"
+  ;
 -- 2.4 Select all the data of employees whose last name is "Smith" or "Doe".
+  select *
+  from Employees
+  where lastname in ("Smith","Doe")
+  ;
 -- 2.5 Select all the data of employees that work in department 14.
+  select *
+  from Employees
+  where department = 14
+  ;
 -- 2.6 Select all the data of employees that work in department 37 or department 77.
+  select * 
+  from Employees
+  where department in (37,77)
+  ;
 -- 2.7 Select all the data of employees whose last name begins with an "S".
+  select *
+  from Employees
+  where lastname like "S%"
+  ;
 -- 2.8 Select the sum of all the departments' budgets.
+  select sum(budgets)
+  from Departments
+  ;
+  
 -- 2.9 Select the number of employees in each department (you only need to show the department code and the number of employees).
+  select Department,count(*)
+  from Employees
+  group by Department
+  ;
+  
 -- 2.10 Select all the data of employees, including each employee's department's data.
+  select a.*,b.*
+  from Employees a 
+  join Departments b
+  where a.department = b.code
+  ;
+  select a.*, b.* from Employees a join Departments b on a.department = b.code;
+  
 -- 2.11 Select the name and last name of each employee, along with the name and budget of the employee's department.
+  select a.name,a.lastname,b.budget
+  from Employees a
+  join Departments b
+  on a.department = b.code
+  ;
+  
+  select a.name, a.lastname, b.name Department_name, b.Budget
+from Employees a join Departments b
+on a.department = b.code;
 -- 2.12 Select the name and last name of employees working for departments with a budget greater than $60,000.
 -- 2.13 Select the departments with a budget larger than the average budget of all the departments.
 -- 2.14 Select the names of departments with more than two employees.
